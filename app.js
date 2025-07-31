2e434a473ba8ae316358480bf9875e6255fb9e37
Juanfle <juanfleming2006@gmail.com>
2025-07-31 13:37:38 -0300

    lógica de app básica lista

--- app.js
+++ app.js
@@ -1,2 +1,36 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
+let listaDeAmigos = [];
+
+function generarNumeroAleatorio() {
+    return Math.floor(Math.random() * listaDeAmigos.length);
+}
+
+function agregarAmigo() {
+    let nombreAmigo = document.querySelector('#amigo').value.trim();
+    if (nombreAmigo === '') {
+        alert("Por favor, ingresa un nombre válido.");
+        return;
+    }
+    document.querySelector('#listaAmigos').innerHTML += `<li>${nombreAmigo}</li>`;
+    listaDeAmigos.push(nombreAmigo);
+    document.querySelector('#amigo').value = '';
+    console.log(listaDeAmigos);
+}
+
+function sortearAmigo() {
+    if (listaDeAmigos.length === 0) {
+        alert("Por favor, agrega al menos un amigo antes de sortear.");
+        return;
+    }
+
+    console.log("Son " + listaDeAmigos.length);
+    let indiceAleatorio = generarNumeroAleatorio();
+    let amigoSecreto = listaDeAmigos[indiceAleatorio];
+    document.querySelector('#resultado').innerHTML = `Tu amigo secreto es: ${amigoSecreto}`;
+
+    document.querySelector('#amigo').value = '';
+    document.querySelector('#listaAmigos').value = '';
+    document.querySelector('#resultado').style.display = 'block';
+    listaDeAmigos = [];
+}
